a14cada94baed2dfae1c7246f51c9f69bac0c7dc
Abhiroop <asiamgenius@gmail.com>
2013-07-11 00:08:38 +0530

    thirteenth commit

--- src/DataAccess.java
+++ src/DataAccess.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -17,7 +19,6 @@ public class DataAccess
 			MySQLAdaptor dba = new MySQLAdaptor("localhost","Reactome","root","passworld",3306);
 			@SuppressWarnings("unchecked")
 			Collection<GKInstance> instances=dba.fetchInstanceByAttribute(ReactomeJavaConstants.Reaction, ReactomeJavaConstants._displayName,"like", query);
-			System.out.println("Results:"+instances.size());
 			for(GKInstance instance:instances)
 			{
 				GKInstance species=(GKInstance)instance.getAttributeValue(ReactomeJavaConstants.species);
@@ -39,7 +40,6 @@ public class DataAccess
 			MySQLAdaptor dba = new MySQLAdaptor("localhost","Reactome","root","passworld",3306);
 			@SuppressWarnings("unchecked")
 			Collection<GKInstance> instances=dba.fetchInstanceByAttribute(ReactomeJavaConstants.Pathway, ReactomeJavaConstants._displayName,"like", query);
-			System.out.println("Results:"+instances.size());
 			for(GKInstance instance:instances)
 			{
 				GKInstance species=(GKInstance)instance.getAttributeValue(ReactomeJavaConstants.species);
@@ -61,7 +61,6 @@ public class DataAccess
 			MySQLAdaptor dba = new MySQLAdaptor("localhost","Reactome","root","passworld",3306);
 			@SuppressWarnings("unchecked")
 			Collection<GKInstance> instances=dba.fetchInstanceByAttribute(ReactomeJavaConstants.Disease, ReactomeJavaConstants._displayName,"like", query);
-			System.out.println("Results:"+instances.size());
 			for(GKInstance instance:instances)
 			{
 				GKInstance species=(GKInstance)instance.getAttributeValue(ReactomeJavaConstants.species);
@@ -75,36 +74,16 @@ public class DataAccess
 		}
 		return result;
 	}
-	public static void main(String [] args)
+	public static void main(String [] args)throws Exception
 	{
-			String query="%Apoptosis%";
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String j=br.readLine();
+			String query=("%"+j+"%");
 			List<String> reaction=Reaction(query);
 			List<String> pathway=Pathway(query);
 			List<String> disease=Disease(query);
-			System.out.println(reaction.size());
-			System.out.println(pathway.size());
-			System.out.println(disease.size());
+			for (String s : reaction) { System.out.println(s); }
+			for (String s : pathway) { System.out.println(s); }
+			for (String s : disease) { System.out.println(s); }
 	}
 }
-/*
-public static void main(String [] args)
-{
-	try
-	{
-		MySQLAdaptor dba = new MySQLAdaptor("localhost","Reactome","root","passworld",3306);
-		String query="%Diabetes%";
-		@SuppressWarnings("unchecked")
-		Collection<GKInstance> instances=dba.fetchInstanceByAttribute(ReactomeJavaConstants.Disease, ReactomeJavaConstants._displayName,"like", query);
-		System.out.println("Results:"+instances.size());
-		for(GKInstance instance:instances)
-		{
-			GKInstance species=(GKInstance)instance.getAttributeValue(ReactomeJavaConstants.species);
-			System.out.println(instance + "[" +species.getDisplayName()+"]");
-		}
-		
-	}
-	catch(Exception e)
-	{
-		e.printStackTrace();
-	}
-*/
